767bcd1177cdc4e68cf2e9de0112d152d5fdca3f
mubashar <zeeshan64kb@gmail.com>
2020-12-04 03:54:55 +0500

    completed... bootcamp9

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Nav from './NavBar';
+import { makeStyles} from '@material-ui/core/styles';
+import {Paper ,TextField,Button,CircularProgress} from '@material-ui/core';
+//import { CenterFocusStrong } from '@material-ui/icons';
+
+
+const useStyles = makeStyles((theme) => ({
+ 
+  paper: {
+    width:400,
+    marginTop: 20,
+    margin: '0 auto',
+    textAlign: 'center',
+    padding: 10,
+
+  },
+  input_field:{
+    width:'98%',
+    marginBottom:'10px',
+  },
+  Circular:{
+    marginTop:10,
+  }
+}));
+
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className={classes.root}>
+      <Nav />
+      <Paper elevation={3} className={classes.paper} >
+      
+        <h1>LogIn </h1>
+
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField  label="Enter Email" className={classes.input_field}/>
+          <TextField  label="Enter Password" className={classes.input_field}/>
+          <Button variant="contained" color="primary" className={classes.input_field} >
+             Primary
+          </Button>
+        </form>
+        <CircularProgress className={classes.Circular} value={50} />
+      </Paper>
+    
     </div>
+
+
   );
 }
 
